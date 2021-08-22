435289753760e063b97333b39c7daa764223969f
YoussefZayed <Youssef.ah.Zayed@gmail.com>
2021-08-21 23:51:03 -0400

    updated UI

--- UnityARena/Assets/ARTapToPlace.cs
+++ UnityARena/Assets/ARTapToPlace.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class ARTapToPlace : MonoBehaviour
 {
@@ -16,6 +18,11 @@ public class ARTapToPlace : MonoBehaviour
     private Vector2 touchPosition;
     private float scaleAmount = 0.0008f;
     private float rotateAmount = 1f;
+    public Text txt;
+    
+
+    private List<Tuple<string,string>> productInfo;
+    private int currentItem = 0;
 
     static List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
@@ -23,10 +30,16 @@ public class ARTapToPlace : MonoBehaviour
     {
         _arRaycastManager = GetComponent<ARRaycastManager>();
         gameObjectToInstatiate = Monitor;
+        Tuple<string, string>[] data = { new Tuple<string, string>("Monitor","Asus 27inch LCD" ),
+                            new Tuple<string, string>("TV","Samsung 57inch LCD" ),
+                            new Tuple<string, string>("Monitor","Samsung 27inch LCD" ),new Tuple<string, string>("Monitor","Acer 27inch LCD" ),new Tuple<string, string>("TV","TCL 57inch LCD" ) };
+        productInfo = new List<Tuple<string, string>>(data);
+        currentItem = -1;
+        nextItem(true);
     }
 
     bool TryGetTouchPosition(out Vector2 touchPosition) {
-        if (Input.touchCount ==1) {
+        if (Input.touchCount ==1 && EventSystem.current.IsPointerOverGameObject()) {
             touchPosition = Input.GetTouch(0).position;
             return true;
         }
@@ -57,14 +70,27 @@ public class ARTapToPlace : MonoBehaviour
             if (spawnedObject == null)
             {
                 spawnedObject = Instantiate(gameObjectToInstatiate, hitPose.position, hitPose.rotation);
+                try
+                {
+                    spawnedObject.transform.GetChild(0).gameObject.transform.GetChild(0).transform.rotation = hitPose.rotation;
+                    var childrotate = spawnedObject.transform.GetChild(0).gameObject.transform.GetChild(0).transform.eulerAngles;
+                    spawnedObject.transform.GetChild(0).gameObject.transform.GetChild(0).transform.eulerAngles = new Vector3((float)(childrotate.x), (float)(childrotate.y + 90), (float)(childrotate.z)); ;
+                    
+                }
+                catch (Exception)
+                {
+
+                }
             }
             else {
                 spawnedObject.transform.position = hitPose.position;
                 try
                 {
                     spawnedObject.transform.GetChild(0).gameObject.transform.GetChild(0).transform.rotation = hitPose.rotation;
-                    var childrotate = spawnedObject.transform.GetChild(0).gameObject.transform.GetChild(0).transform.eulerAngles;
-                    spawnedObject.transform.GetChild(0).gameObject.transform.GetChild(0).transform.eulerAngles = new Vector3((float)(childrotate.x ), (float)(childrotate.y + 90), (float)(childrotate.z)); ;
+                        var childrotate = spawnedObject.transform.GetChild(0).gameObject.transform.GetChild(0).transform.eulerAngles;
+                        spawnedObject.transform.GetChild(0).gameObject.transform.GetChild(0).transform.eulerAngles = new Vector3((float)(childrotate.x), (float)(childrotate.y + 90), (float)(childrotate.z)); ;
+                   
+                    
                 }
                 catch (Exception)
                 {
@@ -74,21 +100,48 @@ public class ARTapToPlace : MonoBehaviour
         }
     }
 
-    public void nextItem() {
-        if (gameObjectToInstatiate == Monitor)
+    public void nextItem(bool next) {
+        if (next)
         {
-            gameObjectToInstatiate = Tv;
+            if (currentItem + 1 >= productInfo.Count)
+            {
+                currentItem = 0;
+            }
+            else
+            {
+                currentItem += 1;
+            }
+        }
+        else
+        {
+            if (currentItem - 1 < 0)
+            {
+                currentItem = productInfo.Count - 1;
+            }
+            else
+            {
+                currentItem -= 1;
+            }
+        }
+        var newGameObject = gameObjectToInstatiate;
+        if (productInfo[currentItem].Item1 == "Monitor")
+        {
+            newGameObject = Monitor;
         }
         else {
-            gameObjectToInstatiate = Monitor;
+            newGameObject = Tv;
         }
+        txt.GetComponent<UnityEngine.UI.Text>().text = productInfo[currentItem].Item2;
         try
         {
-            var curPose = spawnedObject.transform.position;
-            var curRote = spawnedObject.transform.GetChild(0).gameObject.transform.GetChild(0).transform.rotation;
-            Destroy(spawnedObject);
-            spawnedObject = null;
-            spawnedObject = Instantiate(gameObjectToInstatiate, curPose, curRote);
+            if (newGameObject != gameObjectToInstatiate)
+            {
+                gameObjectToInstatiate = newGameObject;
+                var curPose = spawnedObject.transform.position;
+                var curRote = spawnedObject.transform.GetChild(0).gameObject.transform.GetChild(0).transform.rotation;
+                Destroy(spawnedObject);
+                spawnedObject = null;
+            }
         }
         catch (Exception)
         {
